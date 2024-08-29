b0c820634812a3f44bdaf16a0f39374c59a69c37
jangamswarupa-595 <swarupajangam9@gmail.com>
2024-08-29 20:11:17 +0530

    Regex

--- Reg2.java
+++ Reg2.java
@@ -1,6 +1,17 @@
 public class Reg2{
-// Match Repeated Words in a Sentence
+    public static void main(String[] args) {
+    // Match Repeated Words in a Sentence
+    String s1="This is is a test test sentence";
+    String[] s=s1.split("\\s+");
+   for(int i=0;i<s.length;i++){
+    for(int j=i+1;j<s.length;j++){
+        if(s[i].equals(s[j])){
+            System.out.println("Repeated word: " + s[i]);
+        }
+    }
+   }
 // Problem: Write a regex to find any word that appears more than once in a sentence.
 // Example Input: "This is is a test test sentence"
 // Expected Matches: "is", "test"
+}
 }
--- Reg4.java
+++ Reg4.java
@@ -1,6 +1,19 @@
 public class Reg4{
+    public static void main(String[] args) {
 // Match a Decimal Number with Optional Leading and Trailing Zeros
+String[] s={ "007.89", "0.123", "3.14", "100", "000.000"};
+String s1="^0*[0-9]+(\\.0*[0-9]+)?$";
+  for(String s3:s){
+    if(s3.matches(s1)){
+        System.out.println("If matches: "+s3);
+    }
+    else{
+        System.out.println("If does not matches: "+s3);
+    }
+  }
+
 // Problem: Create a regex to match a decimal number that allows for optional leading and trailing zeros.
 // Example Input: "007.89", "0.123", "3.14", "100", "000.000"
 // Expected Matches: "007.89", "0.123", "3.14", "100", "000.000"
+}
 }
