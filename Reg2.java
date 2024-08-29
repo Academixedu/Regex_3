2e295a08442ffe2ce63c59d84c6093e604be6be0
Ritesh047 <riteshkumarbehera43@gmail.com>
2024-08-29 19:50:42 +0530

    regex

--- Reg2.java
+++ Reg2.java
@@ -1,6 +1,23 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Reg2{
 // Match Repeated Words in a Sentence
 // Problem: Write a regex to find any word that appears more than once in a sentence.
 // Example Input: "This is is a test test sentence"
 // Expected Matches: "is", "test"
+        public static void main(String[] args) {
+            String s = "This is is a test test sentence";
+            String regex = "\\b(\\w+)\\b(?=.*\\b\\1\\b)";
+
+             Pattern pattern = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
+
+        // Create a matcher for the input string
+        Matcher matcher = pattern.matcher(s);
+
+        // Find and print all repeated words
+        while (matcher.find()) {
+            System.out.println("Repeated word: " + matcher.group(1));
+        }
+        }
 }
