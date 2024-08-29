d27550b5af8ef9610361b1b0b0852cdfd360007b
Chanakya-dev <Chanakyamanas123@gmail.com>
2024-08-29 06:01:56 +0530

    Create Reg1.java

---
+++ Reg1.java
@@ -0,0 +1,6 @@
+public class Reg1{
+//  Validate a Basic Hexadecimal Color Code
+// Problem: Create a regex to match valid hexadecimal color codes in the format "# followed by exactly 3 or 6 hexadecimal digits".
+// Example Input: "#FFF", "#123456", "#ABC", "#GHI"
+// Expected Matches: "#FFF", "#123456", "#ABC"
+}
