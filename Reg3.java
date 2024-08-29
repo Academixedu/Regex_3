f98b6c8fa2d94a2a9aee262693c2dc0fc34358d2
Shreyash000007 <shreyasharg@gmail.com>
2024-08-29 20:08:27 +0530

    Shreyash

--- Reg3.java
+++ Reg3.java
@@ -1,6 +1,33 @@
-public class Reg3{
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Reg3
+{
 // Validate a Positive Integer
 // Problem: Create a regex to match strings that represent a positive integer.
 // Example Input: "123", "007", "0", "-123"
 // Expected Matches: "123", "007"
+public void RegEx ()
+{
+    String input ="123";
+    String string="^(0|[1-9][0-9])";
+    Pattern pattern =Pattern.compile(string);
+    Matcher match=pattern.matcher(input);
+
+
+    if (match.find()) 
+    {
+       System.out.println(input+" Positive Number");    
+    }
+    else
+    {
+        System.out.println(input+ " Negative Number");
+    }
+
+}
+public static void main(String[] args) 
+{
+  Reg3 aobj=new Reg3();
+  aobj.RegEx();    
+}
 }
--- Reg4.java
+++ Reg4.java
@@ -1,6 +1,35 @@
-public class Reg4{
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Reg4
+{
 // Match a Decimal Number with Optional Leading and Trailing Zeros
 // Problem: Create a regex to match a decimal number that allows for optional leading and trailing zeros.
 // Example Input: "007.89", "0.123", "3.14", "100", "000.000"
 // Expected Matches: "007.89", "0.123", "3.14", "100", "000.000"
+public void RegEx()
+{
+   Scanner s=new Scanner(System.in);
+   System.out.println("Enter The Value => ");
+   String string="^[0-9]{1,3}[.]?[0-9]{1,3}";
+   Pattern p=Pattern.compile(string);
+   Matcher matcher=p.matcher(string);
+
+   if (matcher.matches()) 
+   {
+     System.out.println(string +" Input is Valid"); 
+   }
+   else
+   {
+    System.out.println(string+" Input is Invalid");
+   }
+}
+public static void main(String[] args) 
+{
+    Reg4 aobj=new Reg4();
+    aobj.RegEx();
+
+}
+
 }
