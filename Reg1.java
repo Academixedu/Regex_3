3ac2359ba1bb72ead3b95ee359bbcc575459fa04
Pavani23678 <pavaniburadhakavi@gmail.com>
2024-08-29 19:05:23 +0530

    regex3

--- Reg1.java
+++ Reg1.java
@@ -1,4 +1,21 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Reg1{
+    public static void main(String[] args) {
+        String pattern="^#([A-Fa-f0-9]{3}|[A-Fa-f0-9]{6})$";
+        String name="#1234Ab";
+        Pattern p = Pattern.compile(pattern);
+    Matcher m = p.matcher(name);
+    if(m.find())
+    {
+        System.out.println("valid");
+    }
+    else{
+        System.out.println("invalid");
+    }
+
+    }
 //  Validate a Basic Hexadecimal Color Code
 // Problem: Create a regex to match valid hexadecimal color codes in the format "# followed by exactly 3 or 6 hexadecimal digits".
 // Example Input: "#FFF", "#123456", "#ABC", "#GHI"
