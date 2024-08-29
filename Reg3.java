c6f634a6c872a3f756de83aea69245b480946d5a
Chanakya-dev <Chanakyamanas123@gmail.com>
2024-08-29 06:04:27 +0530

    Create Reg3.java

---
+++ Reg3.java
@@ -0,0 +1,6 @@
+public class Reg3{
+// Validate a Positive Integer
+// Problem: Create a regex to match strings that represent a positive integer.
+// Example Input: "123", "007", "0", "-123"
+// Expected Matches: "123", "007"
+}
