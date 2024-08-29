d132c67ac8595d047dfa5336e5a249cacbd38783
saikumar34892 <saikumarmatta3821@gmail.com>
2024-08-29 17:55:29 +0530

    from sai

--- Reg2.java
+++ Reg2.java
@@ -1,6 +1,22 @@
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 public class Reg2{
 // Match Repeated Words in a Sentence
 // Problem: Write a regex to find any word that appears more than once in a sentence.
 // Example Input: "This is is a test test sentence"
 // Expected Matches: "is", "test"
+private static void appears(String value){
+String pa= "\\\\b(\\\\w+)\\\\b(?=.*\\\\b\\\\1\\\\b)";
+ Pattern p=Pattern.compile(pa);
+ Matcher m=p.matcher(value);
+ while(m.find()){
+    System.out.println("not repeat values "+m.group(1));
+ }
+
+}
+
+public static void main(String[] args) {
+    String s="This is is a test test sentence";
+    appears(s);
+}
 }
--- Reg4.java
+++ Reg4.java
@@ -1,6 +1,40 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 public class Reg4{
 // Match a Decimal Number with Optional Leading and Trailing Zeros
 // Problem: Create a regex to match a decimal number that allows for optional leading and trailing zeros.
 // Example Input: "007.89", "0.123", "3.14", "100", "000.000"
 // Expected Matches: "007.89", "0.123", "3.14", "100", "000.000"
+
+
+
+
+    public static void decimalnbeumr(String input) {
+        String decimalPattern = "^0*\\d+(\\.\\d+)?$|^0*\\.\\d+$";
+
+        Pattern pattern = Pattern.compile(decimalPattern);
+
+        Matcher m= pattern.matcher(input);
+
+
+        if(m.matches()){
+            System.out.println("matched :"+input);
+        }
+        else{
+            System.out.println("not matched "+input);
+        }
+
+    }
+    public static void main(String[] args) {
+        decimalnbeumr( "007.89");
+        decimalnbeumr(  "0.123");
+        decimalnbeumr( "3.14");
+        decimalnbeumr( "100");
+        decimalnbeumr(  "000.000");
+
+
+
+
+
+    }
 }
