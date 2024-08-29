29d5bd2c5d2696a484341b1129de5b14c586bd91
fayazshaik515 <fayazshaik231104@gmail.com>
2024-08-29 17:36:45 +0530

    from  fayaz

--- Reg2.java
+++ Reg2.java
@@ -1,6 +1,31 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 public class Reg2{
 // Match Repeated Words in a Sentence
 // Problem: Write a regex to find any word that appears more than once in a sentence.
 // Example Input: "This is is a test test sentence"
 // Expected Matches: "is", "test"
+
+
+
+    public static void findRepeatedWords(String word) {
+        String Pa= "\\b(\\w+)\\b\\s+\\b\\1\\b";
+        
+        Pattern p = Pattern.compile(Pa);
+
+        Matcher m = p.matcher(word);
+        
+        if (m.find()) {
+            System.out.println("Repeated word: " + m.group(1));
+        }
+        else{
+            System.out.println("not repeated");
+        }
+    }
+
+    public static void main(String[] args) {
+        String sentence = "This is is a test test sentence";
+
+        findRepeatedWords(sentence);
+    }
 }
--- Reg3.java
+++ Reg3.java
@@ -1,6 +1,33 @@
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 public class Reg3{
 // Validate a Positive Integer
 // Problem: Create a regex to match strings that represent a positive integer.
 // Example Input: "123", "007", "0", "-123"
-// Expected Matches: "123", "007"
+// Expected Matchesimport java.util.regex.Matcher;
+
+    public static void PositiveInteger(String input) {
+        String Pat = "^[1-9]\\d*$|^0\\d+$";
+        
+        Pattern pattern = Pattern.compile(Pat);
+        
+        Matcher matcher = pattern.matcher(input);
+        
+        if(matcher.matches()){
+            System.out.println("matches "+input);
+        }
+        else{
+            System.out.println("not matched "+input);
+        }
+        }
+
+    public static void main(String[] args) {
+        PositiveInteger("123");
+        PositiveInteger("007");
+        PositiveInteger("0");
+        PositiveInteger("-123");
+
+    
+
+}
 }
